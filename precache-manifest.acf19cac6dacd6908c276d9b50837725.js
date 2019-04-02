a0aa8de9c2d3a74dd705b1bdaefcc159f00e9093
Lizette Daluz <lizettejoan@kalibrr.com>
2019-04-02 22:42:16 +0800

    Updates

---
+++ precache-manifest.acf19cac6dacd6908c276d9b50837725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "2726b0f3b368a47d4186",
+    "url": "/static/js/main.38d13cf0.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "2726b0f3b368a47d4186",
+    "url": "/static/css/main.0cb27489.chunk.css"
+  },
+  {
+    "revision": "9a0d6ff14e7938632d737fe254b1c62a",
+    "url": "/index.html"
+  }
+];
